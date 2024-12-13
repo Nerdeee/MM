c20c15bd4b0fdd6bebddb5d6c3dc97bd798aace7
Nerdeee <george.larres@gmail.com>
2024-12-12 18:34:23 -0800

    Began skeleton of project. Will start putting work into projectas semester winds down

---
+++ scraper.py
@@ -0,0 +1,14 @@
+import os
+import requests
+
+# look at differences between requests and beautifulsoup and which one would be best for my needs
+
+class RequestHandler:
+    def __init__(self):
+        return
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
