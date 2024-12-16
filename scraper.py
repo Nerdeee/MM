ac8b1c3702c16363ef1ee52a06c12d564ebcc85a
Nerdeee <george.larres@gmail.com>
2024-12-15 22:59:53 -0800

    Added some code for the reddit API. Will probably use this instead of requests/BS

--- scraper.py
+++ scraper.py
@@ -1,5 +1,9 @@
 import os
 import requests
+import praw # python reddit API wrapper
+import os
+from dotenv import load_dotenv
+from textblob import TextBlob
 
 # look at differences between requests and beautifulsoup and which one would be best for my needs
 
@@ -8,6 +12,14 @@ class RequestHandler:
         return
 
 def main():
+    load_dotenv()
+    reddit = praw.Reddit(
+        client_id=os.getenv('CLIENT_ID'),
+        client_secret=os.getenv('CLIENT_SECRET'),
+        password=os.getenv('PASSWORD'),
+        user_agent=os.getenv('USER_AGENT'),
+        username=os.getenv('USERNAME'),
+    )
     return
 
 if __name__ == "__main__":
