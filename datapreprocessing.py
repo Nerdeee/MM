1e1c4d6b50530c9b0d214f0546a37dadf5d70f84
Nerdeee <george.larres@gmail.com>
2024-12-23 01:06:18 -0800

    Added file for datapreprocessing and pickle file folder once the main script is complete

---
+++ datapreprocessing.py
@@ -0,0 +1,8 @@
+import numpy as np
+import cv2
+import os
+import matplotlib.pyplot as plt
+import random
+import pickle
+
+# this file will be used for all of my data preprocessing once the labels have been made for all of the images
